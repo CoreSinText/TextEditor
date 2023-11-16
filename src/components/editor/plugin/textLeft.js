3664e14ccef7d63839ee16dde0c5681c688a2ca7
CoreSinText <coresintext@gmail.com>
2023-11-16 10:08:25 +0700

    update

--- src/components/editor/plugin/textLeft.js
+++ src/components/editor/plugin/textLeft.js
@@ -5,17 +5,17 @@ import { SecurityEditor } from "../common/secure";
 export default function TextLeft() {
   const selection = document.getSelection();
   const id_editor = document.querySelector("#editor-edit-ujione");
-  let isTagP = selection.focusNode.parentElement;
 
   if (SecurityEditor(selection)) {
     id_editor.focus();
-    switch (searchTagP(isTagP, selection).style.textAlign) {
+    let isTagP = searchTagP(selection.focusNode.parentElement);
+    switch (isTagP.style.textAlign) {
       case "left":
-        searchTagP(isTagP, selection).style.textAlign = "";
+        isTagP.style.textAlign = "";
         break;
 
       default:
-        searchTagP(isTagP, selection).style.textAlign = "left";
+        isTagP.style.textAlign = "left";
         break;
     }
   }
