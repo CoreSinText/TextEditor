6ef2e7d3e15228ab5fd7535ed62b336006290007
CoreSinText <coresintext@gmail.com>
2023-11-16 10:06:18 +0700

    update

--- src/components/editor/common/searchTagP.js
+++ src/components/editor/common/searchTagP.js
@@ -1,9 +1,14 @@
 // @ts-nocheck
-export default function searchTagP(isTagP, selection) {
-  if (isTagP.tagName == "P") {
-    return isTagP;
-  } else {
-    isTagP = selection.focusNode.parentElement;
-    searchTagP();
+export default function searchTagP(isTagP) {
+  let p = isTagP;
+  const trying = 30;
+
+  for (let i = 0; i < trying; i++) {
+    if (p.tagName.toLowerCase() == "p") {
+      break;
+    } else {
+      p = p.parentElement;
+    }
   }
+  return p;
 }
