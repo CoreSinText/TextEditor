165ccd28dbf1b87607121e65f40eec8a97013db3
CoreSinText <coresintext@gmail.com>
2023-11-16 10:09:22 +0700

    update

--- src/components/editor/plugin/textRight.js
+++ src/components/editor/plugin/textRight.js
@@ -4,17 +4,17 @@ import searchTagP from "../common/searchTagP";
 export default function RightText() {
   const selection = document.getSelection();
   const id_editor = document.querySelector("#editor-edit-ujione");
-  let isTagP = selection.focusNode.parentElement;
 
   if (SecurityEditor(selection)) {
     id_editor.focus();
-    switch (searchTagP(isTagP, selection).style.textAlign) {
+    let isTagP = searchTagP(selection.focusNode.parentElement);
+    switch (isTagP.style.textAlign) {
       case "right":
-        searchTagP(isTagP, selection).style.textAlign = "";
+        isTagP.style.textAlign = "";
         break;
 
       default:
-        searchTagP(isTagP, selection).style.textAlign = "right";
+        isTagP.style.textAlign = "right";
         break;
     }
   }
